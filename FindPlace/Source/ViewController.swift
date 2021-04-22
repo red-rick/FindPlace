996eb2e85924d02ba9ac00bb42fd86f3b4f585ad
Dmytro Benedyk <dbenedyk@star.global>
2021-04-22 16:39:13 -0400

    Initial Commit

---
+++ FindPlace/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindPlace
+//
+//  Created by Dmytro Benedyk on 22.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
